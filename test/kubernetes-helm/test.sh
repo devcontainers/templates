289c7a2ddd12f70c85f7795748dd9cd1cf64eebb
Samruddhi Khandale <skhandale@microsoft.com>
2022-10-06 15:59:39 +0000

    update kube

--- test/kubernetes-helm/test.sh
+++ test/kubernetes-helm/test.sh
@@ -3,9 +3,12 @@ cd $(dirname "$0")
 
 source test-utils.sh vscode
 
+# Run common tests
+checkCommon
+
 # Execute .bashrc with the SYNC_LOCALHOST_KUBECONFIG set
-export SYNC_LOCALHOST_KUBECONFIG=true 
-exec bash
+export SYNC_LOCALHOST_KUBECONFIG=true
+exec bash 
 
 # Actual tests
 checkExtension "ms-azuretools.vscode-docker"
