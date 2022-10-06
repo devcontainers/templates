1f69649ed7f3c2b48ea2a31f1491ed6de84ccbcd
Samruddhi Khandale <skhandale@microsoft.com>
2022-10-06 14:56:04 +0000

    remove local feature test

--- test/kubernetes-helm/test.sh
+++ test/kubernetes-helm/test.sh
@@ -3,9 +3,6 @@ cd $(dirname "$0")
 
 source test-utils.sh vscode
 
-# Run common tests
-checkCommon
-
 # Execute .bashrc with the SYNC_LOCALHOST_KUBECONFIG set
 export SYNC_LOCALHOST_KUBECONFIG=true 
 exec bash
