430d9406c9aa82d035b10a3864a1ff373ba78ad6
Samruddhi Khandale <skhandale@microsoft.com>
2022-10-31 09:20:24 -0700

    References to base:* images - Unpin major versions (#26)
    * bump version
    
    * unpin base: major version
    
    * use jammy as default
    
    * fix test - git is now installed by feature
    
    * fix test
    
    * nit

--- test/ubuntu/test-utils-ubuntu.sh
+++ test/ubuntu/test-utils-ubuntu.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 USERNAME=${1:-vscode}
 
 if [ -z $HOME ]; then
@@ -51,13 +50,74 @@ checkOSPackages() {
     LABEL=$1
     shift
     echo -e "\n🧪 Testing $LABEL"
-    if apk info -e "$@"; then 
+    if dpkg-query --show -f='${Package}: ${Version}\n' "$@"; then 
         echo "✅  Passed!"
         return 0
     else
         echoStderr "❌ $LABEL check failed."
         FAILED+=("$LABEL")
         return 1
+    fi
+}
+
+checkCommon()
+{
+    PACKAGE_LIST="apt-utils \
+        git \
+        openssh-client \
+        less \
+        iproute2 \
+        procps \
+        curl \
+        wget \
+        unzip \
+        nano \
+        jq \
+        lsb-release \
+        ca-certificates \
+        apt-transport-https \
+        dialog \
+        gnupg2 \
+        libc6 \
+        libgcc1 \
+        libgssapi-krb5-2 \
+        liblttng-ust1 \
+        libstdc++6 \
+        zlib1g \
+        locales \
+        sudo"
+
+    # Actual tests
+    checkOSPackages "common-os-packages" ${PACKAGE_LIST}
+    checkMultiple "vscode-server" 1 "[ -d $HOME/.vscode-server/bin ]" "[ -d $HOME/.vscode-server-insiders/bin ]" "[ -d $HOME/.vscode-test-server/bin ]" "[ -d $HOME/.vscode-remote/bin ]" "[ -d $HOME/.vscode-remote/bin ]"
+    check "non-root-user" id ${USERNAME}
+    check "locale" [ $(locale -a | grep en_US.utf8) ]
+    check "sudo" sudo echo "sudo works."
+    check "zsh" zsh --version
+    check "oh-my-zsh" [ -d "$HOME/.oh-my-zsh" ]
+    check "login-shell-path" [ -f "/etc/profile.d/00-restore-env.sh" ]
+    check "code" which code
+}
+
+reportResults() {
+    if [ ${#FAILED[@]} -ne 0 ]; then
+        echoStderr -e "\n💥  Failed tests: ${FAILED[@]}"
+        exit 1
+    else 
+        echo -e "\n💯  All passed!"
+        exit 0
+    fi
+}
+
+# Useful for scenarios where UID/GID is not automatically updated - happens in GitHub Actions w/Docker Compose
+fixTestProjectFolderPrivs() {
+    if [ "${USERNAME}" != "root" ]; then
+        TEST_PROJECT_FOLDER="${1:-$SCRIPT_FOLDER}"
+        FOLDER_USER="$(stat -c '%U' "${TEST_PROJECT_FOLDER}")"
+        if [ "${FOLDER_USER}" != "${USERNAME}" ]; then
+            echoStderr "WARNING: Test project folder is owned by ${FOLDER_USER}. Updating to ${USERNAME}."
+            sudo chown -R ${USERNAME} "${TEST_PROJECT_FOLDER}"
+        fi
     fi
 }
 
@@ -85,76 +145,5 @@ checkExtension() {
         echoStderr -e "\n❌ Extension $EXTN_ID not found."
         FAILED+=("$LABEL")
         return 1
-    fi
-}
-
-checkCommon()
-{
-    PACKAGE_LIST="openssh-client \
-        gnupg \
-        procps \
-        lsof \
-        htop \
-        net-tools \
-        psmisc \
-        curl \
-        wget \
-        rsync \
-        ca-certificates \
-        unzip \
-        zip \
-        nano \
-        vim \
-        less \
-        jq \
-        libgcc \
-        libstdc++ \
-        krb5-libs \
-        libintl \
-        libssl1.1 \
-        lttng-ust \
-        tzdata \
-        userspace-rcu \
-        zlib \
-        sudo \
-        coreutils \
-        sed \
-        grep \
-        which \
-        ncdu \
-        shadow \
-        strace \
-        man-pages"
-
-    # Actual tests
-    checkOSPackages "common-os-packages" ${PACKAGE_LIST}
-    checkMultiple "vscode-server" 1 "[ -d $HOME/.vscode-server/bin ]" "[ -d $HOME/.vscode-server-insiders/bin ]" "[ -d $HOME/.vscode-test-server/bin ]" "[ -d $HOME/.vscode-remote/bin ]" "[ -d $HOME/.vscode-remote/bin ]"
-    check "non-root-user" id ${USERNAME}
-    check "sudo" sudo echo "sudo works."
-    check "zsh" zsh --version
-    check "oh-my-zsh" [ -d "$HOME/.oh-my-zsh" ]
-    check "login-shell-path" [ -f "/etc/profile.d/00-restore-env.sh" ]
-    check "code" which code
-}
-
-reportResults() {
-    if [ ${#FAILED[@]} -ne 0 ]; then
-        echoStderr -e "\n💥  Failed tests: ${FAILED[@]}"
-        exit 1
-    else 
-        echo -e "\n💯  All passed!"
-        exit 0
-    fi
-}
-
-# Useful for scenarios where UID/GID is not automatically updated - happens in GitHub Actions w/Docker Compose
-fixTestProjectFolderPrivs() {
-    if [ "${USERNAME}" != "root" ]; then
-        TEST_PROJECT_FOLDER="${1:-$SCRIPT_FOLDER}"
-        FOLDER_USER="$(stat -c '%U' "${TEST_PROJECT_FOLDER}")"
-        if [ "${FOLDER_USER}" != "${USERNAME}" ]; then
-            echoStderr "WARNING: Test project folder is owned by ${FOLDER_USER}. Updating to ${USERNAME}."
-            sudo chown -R ${USERNAME} "${TEST_PROJECT_FOLDER}"
-        fi
     fi
 }
