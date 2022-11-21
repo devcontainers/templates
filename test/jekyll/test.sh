ab4078f0d78d2008d89ad04f00cae6142dedcf5c
Samruddhi Khandale <skhandale@microsoft.com>
2022-11-21 11:42:33 -0800

    Removes node option (#59)
    * Remove node option
    
    * update tests
    
    * update java version

--- test/jekyll/test.sh
+++ test/jekyll/test.sh
@@ -12,7 +12,6 @@ check "gem" gem --version
 check "ruby" ruby --version
 check "bundler" bundler --version
 check "github-pages" github-pages --version
-check "nvm-node" bash -c ". /usr/local/share/nvm/nvm.sh && node --version"
 
 # Report result
 reportResults
