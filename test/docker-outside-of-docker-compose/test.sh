17f2f57fed1860450c52035e387da1cd83f1bb8c
Samruddhi Khandale <skhandale@microsoft.com>
2022-11-29 16:06:58 -0800

    Refactor `docker-outside-of-docker-compose` with Features (#70)
    * Update: docker-outside-of-docker-compose
    
    * nit

--- test/docker-outside-of-docker-compose/test.sh
+++ test/docker-outside-of-docker-compose/test.sh
@@ -14,6 +14,7 @@ checkCommon
 checkExtension "ms-azuretools.vscode-docker"
 check "docker" docker ps -a
 check "docker-compose" docker-compose --version
+check "docker-init-exists" ls -la /usr/local/share/docker-init.sh
 
 # Report result
 reportResults
