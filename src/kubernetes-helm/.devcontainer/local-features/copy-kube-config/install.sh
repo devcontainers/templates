0dc6fd09f04d5e71258e1e9494919f7d9f32ebb8
Chuck Lantz <chuck_lantz@hotmail.com>
2022-12-15 23:19:41 +0000

    Indicate Dockerfile can be used, simplify now that image metadata is live

--- src/kubernetes-helm/.devcontainer/local-features/copy-kube-config/install.sh
+++ src/kubernetes-helm/.devcontainer/local-features/copy-kube-config/install.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-USERNAME=${USERNAME}
+USERNAME="${USERNAME:-"${_REMOTE_USER}"}"
 
 # Script copies localhost's ~/.kube/config file into the container and swaps out 
 # localhost for host.docker.internal on bash/zsh start to keep them in sync.
 cp copy-kube-config.sh /usr/local/share/
 
 chown ${USERNAME}:root /usr/local/share/copy-kube-config.sh
-echo "source /usr/local/share/copy-kube-config.sh" | tee -a /root/.bashrc /root/.zshrc /home/${USERNAME}/.bashrc >> /home/${USERNAME}/.zshrc
+echo "source /usr/local/share/copy-kube-config.sh" | tee -a /root/.bashrc >> /root/.zshrc
+if [ ! -z "${USERNAME}" ] && [ "${USERNAME}" != "root" ]; then
+    echo "source /usr/local/share/copy-kube-config.sh" | tee -a /home/${USERNAME}/.bashrc >> /home/${USERNAME}/.zshrc
+fi
