880f1878cf6b248cf866b21f3d0fd25472e21822
Thomas Owens <thomas.j.owens@gmail.com>
2023-09-22 21:53:32 -0400

    Added explicit checks for write permission.
    Explicitly checking the RVM gems directories for the current user's
    write permissions in the tests. This is also passing locally within a
    container built using the updated configuration and fails within a
    container build using the current baseline and in GitHub Actions.

--- test/ruby-rails-postgres/test.sh
+++ test/ruby-rails-postgres/test.sh
@@ -3,8 +3,6 @@ cd $(dirname "$0")
 
 source test-utils.sh vscode
 
-whoami
-
 # Remote - Containers does not auto-sync UID/GID for Docker Compose,
 # so make sure test project prvs match the non-root user in the container.
 fixTestProjectFolderPrivs
@@ -14,6 +12,8 @@ checkCommon
 
 # Run devcontainer specific tests
 check "rails" rails --version
+check "user has write permission to rvm gems" [ -w /usr/local/rvm/gems ]
+check "user has write permission to rvm gems default" [ -w /usr/local/rvm/gems/default ]
 check "user can install gems" gem install github-markup
 
 # Report result
