28625b912b7364499a237990179b21838b73827e
sireeshajonnalagadda <sireeshajonnalagadda@github.com>
2026-02-05 10:13:40 +0000

    update gem installation checks in test script

--- test/ruby-rails-postgres/test.sh
+++ test/ruby-rails-postgres/test.sh
@@ -15,7 +15,10 @@ check "rails" rails --version
 check "rails installation path" gem which rails
 check "user has write permission to rvm gems" [ -w /usr/local/rvm/gems ]
 check "user has write permission to rvm gems default" [ -w /usr/local/rvm/gems/default ]
-check "user can install gems" gem install github-markup
+# Verify bundler is available in the gem list
+check "user can install gems" gem list | grep -q bundler
+# Verify user can successfully install a gem package
+check "user can install gems" gem install json --no-document
 
 # Report result
 reportResults
