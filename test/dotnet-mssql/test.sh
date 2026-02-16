1e92ed4ff7ea42674e75e4fdfd175a6e62b81fc8
Kaniska <kaniska244@github.com>
2026-02-16 16:28:13 +0000

    Fixing tests

--- test/dotnet-mssql/test.sh
+++ test/dotnet-mssql/test.sh
@@ -17,8 +17,9 @@ check "dotnet" dotnet --info
 check "nuget" dotnet restore
 check "msbuild" dotnet msbuild
 rm -rf obj bin
-check "nvm" bash -c ". /usr/local/share/nvm/nvm.sh && nvm install 10"
+check "nvm" bash -c ". /usr/local/share/nvm/nvm.sh && nvm install 24"
 check "nvm-node" bash -c ". /usr/local/share/nvm/nvm.sh && node --version"
+check "corepack" bash -c ". /usr/local/share/nvm/nvm.sh && corepack enable"
 check "yarn" bash -c ". /usr/local/share/nvm/nvm.sh && yarn --version"
 
 # Report result
